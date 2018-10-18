0306e845cc071e6e75ca59e644e7be96fd00b676
lilyhuong <44270876+lilyhuong@users.noreply.github.com>
2018-10-18 19:06:41 +0200

    Update Ex2. Nguyen Thi Huong. TD2.py

--- Ex2. Nguyen Thi Huong. TD2.py
+++ Ex2. Nguyen Thi Huong. TD2.py
@@ -1,9 +1,9 @@
-a = int (input ("Entre votre numbres de copies:"))
+a = int(input("Entre votre numbres de copies:"))
 Total = 0
 if (a <= 10):
-    total = 10*a
-elif (a<= 30):
-    total = 10*10 + (a-10)*9
-else :
-    total = 10*10 + 20*9 + (a-30)*8
+    total = 10 * a
+elif (a <= 30):
+    total = 10 * 10 + (a - 10) * 9
+else:
+    total = 10 * 10 + 20 * 9 + (a - 30) * 8
 print ("total =", total)
