332ca49e9706f942b56cb30e0e378c3c759e637b
lilyhuong <44270876+lilyhuong@users.noreply.github.com>
2018-10-21 18:06:14 +0200

    Add files via upload

---
+++ Ex6.TD2. Nguyen Thi Huong.py
@@ -0,0 +1,12 @@
+def Huongbelle(array):
+    output = []
+    for n in array:
+        output.append(n+1)
+    return output
+
+nombres = [3,4,2,2,6,3,6,6,5,7]
+nouveaux_nombres = Huongbelle(nombres)
+print(nombres)
+print(nouveaux_nombres) 
+
+
